2fabef5f8df0a0f48bf4c679bb6dc033c2f63c3f
Muhammad Al-farisy <108552261+mhdalfarisy@users.noreply.github.com>
2026-03-14 21:39:14 +0700

    Re-uploading everything to fix deployment

--- src/content_option.js
+++ src/content_option.js
@@ -149,7 +149,7 @@ const contactConfig = {
 const socialprofils = {
     github: "https://github.com",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/m-alfarisy97/",
     twitter: "https://twitter.com",
 };
 export {
